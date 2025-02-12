66c56d72d67e600e8ab4350230ab19381cc100eb
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2025-02-12 21:07:58 +0100

    Added Emacs major mode for unigraph for highlighting in unit.cmake files

---
+++ unigraph-mode.el
@@ -0,0 +1,26 @@
+(require 'cmake-mode)
+
+(defun unigraph-keywords ()
+  '("NAME" "TYPE" "SOURCES" "HEADERS" "DEPEND" "INCLUDE_DIRS"
+    "TEST_SOURCES" "NOLINK_DEPEND"))
+
+(defun unigraph-font-lock-keywords ()
+  (list
+   ;; Highlight keywords like SOURCES, HEADERS, DEPEND
+   `(,(regexp-opt (unigraph-keywords) 'symbols) . font-lock-keyword-face)
+
+   ;; Highlight platform annotations like :windows, :darwin
+   '(":\\([a-zA-Z0-9-_]+\\(?:\\(:[a-zA-Z0-9-_]+\\)*\\)\\)" . font-lock-preprocessor-face)
+
+   ;; Highlight the 'unigraph_unit' function call itself
+   '("unigraph_unit" . font-lock-function-name-face)))
+
+(define-derived-mode unigraph-mode cmake-mode "Unigraph"
+  "Simple major mode for editing Unigraph unit files."
+  :syntax-table nil ;; Inherit the syntax table of cmake-mode
+  (setq-local font-lock-defaults '(unigraph-font-lock-keywords)))
+
+;; Automatically enable unigraph-mode for unit.cmake files
+(add-to-list 'auto-mode-alist '("unit\\.cmake\\'" . unigraph-mode))
+
+(provide 'unigraph-mode)
